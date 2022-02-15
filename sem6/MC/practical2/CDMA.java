04a624aaea8d7307983b9012336cc4d3e2ad3718
Adnan Shaikh <aa2232783@gmail.com>
2022-02-15 14:51:07 +0530

    just a smol commit

--- sem6/MC/practical2/CDMA.java
+++ sem6/MC/practical2/CDMA.java
@@ -8,7 +8,7 @@ public class CDMA {
 	private int[][] copy;
 	private int[] channel_sequence;
 
-	public void setUp(int[] data, int num_stations)
+	public int[] setUp(int[] data, int num_stations)
 	{
 
 		wtable = new int[num_stations][num_stations];
@@ -42,6 +42,7 @@ public class CDMA {
 				channel_sequence[i] += wtable[j][i];
 			}
 		}
+		return channel_sequence;
 	}
 
 	public void listenTo(int sourceStation, int num_stations)
@@ -53,9 +54,10 @@ public class CDMA {
 			// multiply channel sequence and source station code
 			innerProduct += copy[sourceStation][i] * channel_sequence[i];
 		}
-		
-		System.out.println("The data received is: " +
-							(innerProduct / num_stations));
+		int data = innerProduct / num_stations;
+		if(data == -1) data = 1;
+		else data = 0;
+		System.out.println("The data received is: " + data);
 	}
 
 	public int buildWalshTable(int len, int i1, int i2, int j1,
@@ -74,14 +76,14 @@ public class CDMA {
 				wtable[i1][j1] = 1;
 				wtable[i1][j2] = 1;
 				wtable[i2][j1] = 1;
-				wtable[i2][j2] = 1;
+				wtable[i2][j2] = -1;
 			}
 			else {
 
 				wtable[i1][j1] = -1;
 				wtable[i1][j2] = -1;
 				wtable[i2][j1] = -1;
-				wtable[i2][j2] = +1;
+				wtable[i2][j2] = 1;
 			}
 
 			return 0;
@@ -117,22 +119,29 @@ public class CDMA {
 	public static void main(String[] args)
 	{
 		int num_stations = 4;
-		
+		int[] channel_sequence;
 		int[] data = new int[num_stations];
-		
 		//data bits corresponding to each station
-		data[0] = -1;
-		data[1] = -1;
-		data[2] = 0;
+		data[0] = 0;
+		data[1] = 0;
+		data[2] = 1;
 		data[3] = 1;
-		
+
+		System.out.println("D1 = "+data[0]+ " D2 = "+data[1]+" D3 = "+data[2]+" D4 = "+data[3]);
+		for(int i=0;i<4;i++) if(data[i]==0) data[i] = 1; else data[i] = -1; 
 		CDMA channel = new CDMA();
 		
-		channel.setUp(data, num_stations);
+		channel_sequence = channel.setUp(data, num_stations);
+		System.out.print("Channel Seqeunce: ");
+		for(int C: channel_sequence) System.out.print(C+" ");
+		System.out.println();
 
-		// station you want to listen to
-		int sourceStation = 3;
+		while(true){
+			System.out.print("Enter the station no. to extract the data: ");
+			// station you want to listen to
+			int sourceStation = new Scanner(System.in).nextInt() - 1;
 
-		channel.listenTo(sourceStation, num_stations);
+			channel.listenTo(sourceStation, num_stations);
+		}
 	}
 }
